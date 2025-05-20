cf14832e02a7dc2115a6b5468946ec37e94eb5b5
Alexandra-Paula <sandamanea05@gmail.com>
2025-05-20 14:14:11 +0300

    Add ApplicationDBContext in DBModel

---
+++ eSport.BusinessLogic/DBModel/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eSport.Domain.Entities.User;
+using eSport.Domain.Entities;
+
+namespace eSport.BusinessLogic.DBModel
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+            base("name = eGymLife")
+        {
+        }
+        public DbSet<UDbTable> UDbTable { get; set; }
+
+    }
+
+
+}
+
+
